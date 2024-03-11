5b0534e41644fbe389708017e5e893f73ef0152f
Marcus Haukelid Larsen <mhl@rtx.dk>
2024-03-11 20:21:57 +0100

    Updated bar to paths

--- bar/launch.py
+++ bar/launch.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python3
 
 from pathlib import Path
-import psutil
 from subprocess import run
 
 EXEC = Path("/usr/bin/eww")
-BAR_CFG  = Path("~/.config/eww/bar")
+BAR_CFG  = Path(f"{Path.home()}/.config/eww/bar")
 
-# Start eww daemon
-isEwwDeamonAlive = False
-
-for p in psutil.process_iter(["name"]):
-    if p.info["name"] == "eww":
-        isEwwDeamonAlive = True
-
-if not isEwwDeamonAlive:
-    run([str(EXEC), "-c", str(BAR_CFG), "daemon"])
-
-# Open bar
-def open_bar():
-    run([str(EXEC), "-c", str(BAR_CFG), "open-many", "bar"])
-
-open_bar()
+run([EXEC, "-c", BAR_CFG, "open-many", "bar"])
